950dfe2a0671f368adabba52413ed7dc961d7175
roylisto.pradana <roylisto.pradana@grabtaxi.com>
2024-11-19 13:33:58 +0700

    exercise 1.10

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100
+loop = 10
+
+while loop > 0:
+    height = (3 / 5) * height
+    print(round(height, 4))
+    loop = loop - 1
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,27 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+extra_payment = 1000
+# extra_payment_duration = 12
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+month = 0
+
+while principal > 0:
+    month += 1
+    if month <= extra_payment_end_month and month >= extra_payment_start_month:
+        total_payment = payment + extra_payment
+    else:
+        total_payment = payment
+
+    principal = principal * (1 + rate / 12) - total_payment
+    total_paid += total_payment
+    print(month, total_paid, round(principal, 2))
+
+print(f"Total paid: ${total_paid:,.1f}")
+print(f"Number of months: {month}")
